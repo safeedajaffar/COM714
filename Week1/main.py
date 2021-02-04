4e4d02bcb4008ef20ba9c97c78f29bcbb821a725
safeedajaffar <safeedasalam31@gmail.com>
2021-02-04 16:48:17 +0000

    Completed second question

--- Week1/main.py
+++ Week1/main.py
@@ -1,4 +1,14 @@
 
+def second_question():
+    choice= input("Who sees me rolling (cops, wardens, other)?\n")
+    if choice == "cops":
+        print("They see me rolling. They hating. They hoping they gonna catch me riding dirty!")
+    elif choice == "wardens":
+        print("My music's so loud. I'm swanging. They hoping they gonna catch me riding dirty!")
+    else:
+        print("Trying to catch me riding dirty!")
+
+
 def first_question():
     name=input("Who broke my heart?\n")
     out="My first love, "+ name + ", broke my heart for the first time."
@@ -6,6 +16,6 @@ def first_question():
     print("And I was like baby, baby, baby oh.")
 
 
-
 if __name__ == '__main__':
     first_question()
+    second_question()
