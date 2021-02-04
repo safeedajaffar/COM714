c4a44effd64fe09e46a624ff53de3d7a5782014b
safeedajaffar <safeedasalam31@gmail.com>
2021-02-04 16:32:24 +0000

    Completed first question

---
+++ Week1/main.py
@@ -0,0 +1,11 @@
+
+def first_question():
+    name=input("Who broke my heart?\n")
+    out="My first love, "+ name + ", broke my heart for the first time."
+    print(out)
+    print("And I was like baby, baby, baby oh.")
+
+
+
+if __name__ == '__main__':
+    first_question()
